9885a1473e85caf2fba005515a28a75ae7e53a25
fiqrikm15 <fiqrikm15@gmail.com>
2017-03-06 14:06:02 +0700

    belum ada apa apa

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+	<nav class="navigation">
+		<div class="container">
+			<ul>
+			<li><a href="#">Category</a></li>
+			<li><a href="#">lsald</a></li>
+			</ul>
+		</div>
+	</nav>
+</body>
+</html>
