34e1008ca6fc0e4461f4712082d8b88ffe5590c1
fiqrikm15 <fiqrikm15@gmail.com>
2017-03-06 18:50:09 +0700

    karak navigation bar

--- index.php
+++ index.php
@@ -1,18 +1,35 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Uline Store</title>
 	<link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
 	<nav class="navigation">
 		<div class="container">
+			<ul id="top">
+				<li><a href="#"><img src="assets/img/Uline store.png" width="200"></a>
+				<li class="pull-right down"><a href="#">Login</a></li>
+				<li class="pull-right down"><a href="#">Register</a></li>
+				
+			</ul>
 			<ul>
-			<li><a href="#">Category</a></li>
-			<li><a href="#">lsald</a></li>
-			<li><input type="text" placeholder="Search" class="search"></li>
-			<li><a href="#">Login</a></li>
-			<li><a href="#">Register</a></li>
+			  <li class="dropdown">
+			    <a href="#" class="dropbtn">Category</a>
+			    <div class="dropdown-content">
+			      <a href="#">Baju + Celana</a>
+			      <a href="#">Sepatu</a>
+			      <a href="#">Jaket</a>
+			    </div>
+			  	</li>
+			  	<li class="dropdown">
+			    <a href="#" class="dropbtn">Pakaian</a>
+			    <div class="dropdown-content">
+			      <a href="#">Pria</a>
+			      <a href="#">Wanita</a>
+			    </div>
+			  	</li>
+				<li class="pull-right"><input type="text" placeholder="Search" class="search"></li>
 			</ul>
 		</div>
 	</nav>
